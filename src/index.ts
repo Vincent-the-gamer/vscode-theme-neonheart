e9b4afe664f68f87e5adbba4f594860d3261cc43
vincent-the-gamer <1127620453@qq.com>
2024-01-16 22:18:28 +0800

    feat: enhance tab effect.

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ import vscode from "vscode"
 
 export function activate(this: any) {
 	this.extensionName = 'vincent-the-gamer.neonheart';
-	vscode.window.showInformationMessage("NeonHeart Activated!")
 }
 
 export function deactivate() {
-	vscode.window.showInformationMessage("NeonHeart Deactivated!")
+
 }
